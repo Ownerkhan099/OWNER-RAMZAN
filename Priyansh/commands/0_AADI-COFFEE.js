cd646b88b179488e6dd805edefe2023d6bc09e26
Ownerkhan099 <ranasab2430@gmail.com>
2025-05-16 04:13:49 +0500

    Update 0_AADI-COFFEE.js

--- Priyansh/commands/0_AADI-COFFEE.js
+++ Priyansh/commands/0_AADI-COFFEE.js
@@ -3,7 +3,7 @@ module.exports.config = {
         name: "Coffee",
     version: "1.0.1",
         hasPermssion: 0,
-        credits: "AADI BABU", 
+        credits: "𝐙𝐄𝐃𝐈 𝐑𝐀𝐍𝐀 𝐑𝐀𝐌𝐙𝐀𝐍", 
         description: "hihihihi",
         commandCategory: "no prefix",
         usages: "Coffee",
@@ -14,7 +14,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
         var { threadID, messageID } = event;
         if (event.body.indexOf("Coffee")==0 || event.body.indexOf("coffee")==0 || event.body.indexOf("COFFEE ")==0 || event.body.indexOf("cofi")==0) {
                 var msg = {
-                                body: "=𒁍 ⃝𝐎𝐖𝐍𝐄𝐑 ⸙𓆩𝐇𝐀𝐍𝐙𝐔𓆪 𓆣 »❯ \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐎𝐅𝐅𝐄𝐄 \n__________________________________ ",
+                                body: "=𒁍 ⃝𝐎𝐖𝐍𝐄𝐑 ⸙𓆩𝐑𝐀𝐍𝐀 𝐑𝐀𝐌𝐙𝐀𝐍𓆪 𓆣 »❯ \n__________________________________\n\n𝐘𝐚𝐚 𝐋𝐨𝐨 𝐁𝐚𝐁𝐲 𝐂𝐎𝐅𝐅𝐄𝐄 \n__________________________________ ",
                                 attachment: fs.createReadStream(__dirname + `/noprefix/Pinterest-boomerang.mp4`)
                         }
                         api.sendMessage(msg, threadID, messageID);
